8bd90536466e49e3bf3398d759a8dc84d242b8a6
yingmuzhi <cyxscj@126.com>
2022-01-18 08:48:11 +0800

    finish learn_terminal.c

---
+++ learn_terminal.c
@@ -0,0 +1,19 @@
+/*1.first of all，you ought to know one important command about man // 第一个你需要学习的重要命令是man
+ *      man cd
+ *      man mv
+ *      man vim
+ *      man mkdir
+ *      man ls
+ *2.in addition, if you want to know about directory you ought to know about cd and ls// 如果要查找目录，需要使用cd 和 ls命令
+ *      cd ~                根目录
+ *      cd ../              上一级目录
+ *      ls -l
+ *      ls -a               显示所有文件和目录，包括隐藏的
+ *3.创建删除文件和目录
+ *      vim new.c           创建文件
+ *      mkdir ./new
+ *      rm ./new        
+ *4.复制用cp，重命名用mv
+ *
+ *
+ */
