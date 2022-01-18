c706b96cb0b7cf0b5db71cda8ce15542865f7650
yingmuzhi <cyxscj@126.com>
2022-01-18 08:33:00 +0800

    finish

---
+++ README.c
@@ -0,0 +1,10 @@
+/*
+ * 1.first of all, you ought to know how to get SSH on your computer.//你需要先在mac上配置好SSH
+ * 2.secondly,you ought to create a new repository on browser.//你需要使用浏览器在github.com上创建一个新的仓库
+ * 3.in addition, what looks beyond dispute is that you are supposed to open terminal, then use "git clone 'SSH' ".//你需要在mac的terminal中使用cd和mkdir定位到存放文件的位置，使用git clone "SSH", SSH在创建完repository后会提供
+ *      git clone "https://github.com/yingmuzhi/test"
+ * 4.finish your code, return to the main home directory of your repository, use 
+ *      git add .
+ *      git commit -m "fit commit"
+ *      git push
+ *   to finish your first work.
