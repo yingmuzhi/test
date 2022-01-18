ffc2eaf0b37bdf066c35a4d748e5016915571af6
yingmuzhi <cyxscj@126.com>
2022-01-18 09:10:36 +0800

    finish learn_vim.c

---
+++ learn_vim.c
@@ -0,0 +1,13 @@
+/*1.vim具有四种状态，一般我们只会使用前三种状况，normal-mode，insert-mode和command-mode
+ * 2.在normal-mode下
+ *      w按单词跳词前进
+ *      h,j,k,l左下上右
+ *      i该行插入(turn on insert-mode)
+ *      o另起一行插入(turn on insert-mode)
+ *      :命令(turn on command-mode)
+ * 3.在insert-mode下进行代码编辑
+ * 4.在command-mode下使用
+ *      q!退出但不保存
+ *      wq退出并保存
+ *      w保存
+ */
